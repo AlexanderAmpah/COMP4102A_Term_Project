c6666d3b710f171f7002391b65f36c4accccc99b
Anononmos <ainan.kashif@gmail.com>
2024-02-20 23:27:46 -0500

    box processing work in progress

--- in.py
+++ in.py
@@ -7,8 +7,18 @@ Loads grayscale image with filename
 """
 def loadImg(filename):
     # Loads camera input as grayscale
-    # Later performs letter boxing.
+    # Might zoom in 
 
     img = cv2.imread(img, 0)
     
+    return img
+
+
+"""
+Boxes in individual letters i.e., blobs
+"""
+def box_letters(img):
+    blur = cv2.GaussianBlur(img, (7, 7), 1)
+    canny = cv2.Canny(blur, 50, 50)
+    img1 = img.copy()
     ...
